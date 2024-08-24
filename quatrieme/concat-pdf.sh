ea96943f9991f2fdecdcb46537f7582db32099ed
Thibaud Fontanet <tfontanet@free.fr>
2024-08-24 09:27:01 +0200

    exercices livre 4ème

---
+++ quatrieme/concat-pdf.sh
@@ -0,0 +1,5 @@
+# Concatener les cours : (manque "Reperage dans l'espace" et "Pyramides et Cones")
+pdfunite 4-nombres-relatifs-cours.pdf 4-translations-rotations-cours.pdf 4-fractions-cours.pdf 4-puissances-cours.pdf 4-divisibilite-nombrespremiers-cours.pdf 4-triangles-isometriques-cours.pdf 4-calcul-litteral-cours.pdf 4-pythagore-cours.pdf 4-equations-cours.pdf 4-proportionnalite-cours.pdf 4-statistiques-cours.pdf 4-probabilites-cours.pdf cours.pdf
+
+# Concatener les numéros d'exos du livre : (manque "Reperage dans l'espace")
+pdfunite 4-nombres-relatifs-livre.pdf 4-translations-rotations-livre.pdf 4-fractions-livre.pdf 4-puissances-livre.pdf 4-divisibilite-nombrespremiers-livre.pdf 4-triangles-isometriques-livre.pdf 4-calcul-litteral-livre.pdf 4-pythagore-livre.pdf 4-equations-livre.pdf 4-reperage-livre.pdf 4-proportionnalite-livre.pdf 4-statistiques-livre.pdf 4-probabilites-livre.pdf livre.pdf
