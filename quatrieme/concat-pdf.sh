49538672c5391016f0a2f99329a594a982b79750
Thibaud Fontanet <tfontanet@free.fr>
2025-07-13 17:16:42 +0200

    augmenté les interlignes dans mes cours de 4ème pour la mise en livret

--- quatrieme/concat-pdf.sh
+++ quatrieme/concat-pdf.sh
@@ -1,4 +1,4 @@
-# Concatener les cours : (manque "Reperage dans l'espace" et "Pyramides et Cones")
+# Concatener les cours : (manque "Reperage dans l'espace")
 pdfunite 4-couverture-cours.pdf 4-nombres-relatifs-cours.pdf 4-translations-rotations-cours.pdf 4-fractions-cours.pdf 4-puissances-cours.pdf 4-divisibilite-nombrespremiers-cours.pdf 4-triangles-isometriques-cours.pdf 4-calcul-litteral-cours.pdf 4-pythagore-cours.pdf 4-equations-cours.pdf 4-proportionnalite-cours.pdf 4-statistiques-cours.pdf 4-pyramides-cones-cours.pdf 4-probabilites-cours.pdf cours.pdf
 
 # Concatener les numéros d'exos du livre : (manque "Reperage dans l'espace")
