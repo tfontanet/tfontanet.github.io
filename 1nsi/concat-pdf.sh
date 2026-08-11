55b3d993fcdc932f175eb6063a278d546946ec38
Thibaud Fontanet <tfontanet@free.fr>
2026-08-11 22:16:40 +0200

    divers

---
+++ 1nsi/concat-pdf.sh
@@ -0,0 +1,2 @@
+# Concatener les cours : (manque "Reperage dans l'espace")
+pdfunite 1nsi-entier-positif-cours.pdf 1nsi-entier-signe-cours.pdf 1nsi-decimal-flottant-cours.pdf 1nsi-codage-caractere-cours.pdf 1nsi-algebre-boole-cours.pdf 1nsi-architecture-cours.pdf 1nsi-os-cours.pdf 1nsi-reseau-cours.pdf 1nsi-algorithmes-cours.pdf 1nsi-client-serveur-cours.pdf 1nsi-tables-cours.pdf cours.pdf
